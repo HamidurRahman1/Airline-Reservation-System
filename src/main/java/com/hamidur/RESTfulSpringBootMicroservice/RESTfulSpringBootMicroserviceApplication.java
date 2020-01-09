82a2ad6e577560786bc0380ced1785d19ae28ebe
Hamidur Rahman <Hamidur0405@gmail.com>
2020-01-09 11:46:21 -0500

    Initial commit

---
+++ src/main/java/com/hamidur/RESTfulSpringBootMicroservice/RESTfulSpringBootMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hamidur.RESTfulSpringBootMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTfulSpringBootMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTfulSpringBootMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamidur/RESTfulSpringBootMicroservice/RESTfulSpringBootMicroserviceApplicationTests.java
@@ -0,0 +1,4 @@
+package com.hamidur.RESTfulSpringBootMicroservice;
+
+class RESTfulSpringBootMicroserviceApplicationTests {
+}

02de9823d7e1f08474b37b8153903e28d4ff4b80
Hamidur Rahman <Hamidur0405@gmail.com>
2020-01-11 11:34:10 -0500

    adding simple customer

---
+++ src/main/java/com/hamidur/RESTfulSpringBootMicroservice/models/Customer.java
@@ -0,0 +1,58 @@
+package com.hamidur.RESTfulSpringBootMicroservice.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer
+{
+    @Id
+    @Column(name = "customer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer customerId;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    public Customer() {}
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
